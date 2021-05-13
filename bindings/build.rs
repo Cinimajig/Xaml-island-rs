16010c1c398098c4978f96774ca66e2da3e48383
Cinimajig <39052490+Cinimajig@users.noreply.github.com>
2021-05-13 19:36:53 +0200

    Update build.rs

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,13 +1,17 @@
 fn main() {
     windows::build!(
       Windows::UI::Xaml::*,
+      Windows::UI::{
+        Colors, Color
+      },
+      Windows::UI::Xaml::Media::SolidColorBrush,
       Windows::UI::Xaml::Controls::*,
       Windows::UI::Xaml::Hosting::*,
       Windows::Foundation::Uri,
       Windows::Win32::System::WinRT::{RoInitialize, RoUninitialize, RO_INIT_TYPE},
       Windows::Win32::UI::DisplayDevices::RECT,
       Windows::Win32::Graphics::Gdi::{
-        HBRUSH, UpdateWindow, 
+        HBRUSH, UpdateWindow, BeginPaint, EndPaint, TextOutW, PAINTSTRUCT
       },
       Windows::Win32::System::SystemServices::{
         PWSTR, GetModuleHandleW
@@ -17,7 +21,8 @@ fn main() {
         HWND, CreateWindowExW, RegisterClassExW, DefWindowProcW, WNDCLASSEXW, CS_VREDRAW, CS_HREDRAW, LoadIconW, LoadCursorW,
         IDI_APPLICATION, IDC_APPSTARTING, PostQuitMessage, WS_OVERLAPPEDWINDOW, WS_VISIBLE, CW_USEDEFAULT, GetMessageW, TranslateMessage,
         DispatchMessageW, WM_QUIT, WM_CREATE, WS_CHILD, WS_BORDER, WM_DESTROY, WM_CREATE, SetWindowPos, SWP_SHOWWINDOW, WM_SIZE, GetClientRect,
-        MoveWindow, ShowWindow, SW_SHOW
+        MoveWindow, ShowWindow, SW_SHOW, WM_PAINT
       }
     );
 }
+
